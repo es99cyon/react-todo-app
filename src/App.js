0f566b4e513c21277ca3ce5a5ab929776bde83d8
test vm <vmtest@testui-MacBookPro.local>
2023-03-31 21:23:04 +0900

    add asubdir1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList'
+
+const App = () => {
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+      <TodoList />
+      
+    </TodoTemplate>
+  );
+};
+
+export default App;
