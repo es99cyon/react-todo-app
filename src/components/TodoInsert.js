3378041b151197cc1d82a98300d1ab3addccc6fe
test vm <vmtest@testui-MacBookPro.local>
2023-04-05 23:59:01 +0900

    Insert 추가

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,5 +1,14 @@
+import { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
+
+const TodoInseert = () => {
+    const [value, setValue] = useState('');
+
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+    })
+}
 
 const TodoInsert = () => {
     return (
